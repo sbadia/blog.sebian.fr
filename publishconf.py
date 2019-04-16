fe79d8a61d8bbbfc16fae6b711cd4ccc78af1901
Sebastien Badia <seb@sebian.fr>
2019-04-16 16:50:12 +0200

    conf: %s usage in CATEGORY_FEED_RSS is deprecated

--- publishconf.py
+++ publishconf.py
@@ -16,7 +16,7 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/{slug}.rss.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
